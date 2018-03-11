37929ab2c91ba670c791cedd5ad6df8856abde52
Stadzior <Stadzior@gmail.com>
2018-03-11 10:05:27 +0100

    extracting links

--- FSharpCrawler/Program.fs
+++ FSharpCrawler/Program.fs
@@ -1,8 +1,27 @@
 ﻿// Learn more about F# at http://fsharp.org
 
 open System
+open FSharp.Data
+
+let results = HtmlDocument.Load("http://pduch.kis.p.lodz.pl/")
+
+let links = 
+    results.Descendants ["a"]
+    |> Seq.choose (fun x -> 
+           x.TryGetAttribute("href")
+           |> Option.map (fun a -> x.InnerText(), a.Value())
+    )
+
+//let searchResults =
+//    links
+//    |> Seq.filter (fun (name, url) -> 
+//                    name <> "Cached" && name <> "Similar" && url.StartsWith("/url?"))
+//    |> Seq.map (fun (name, url) -> name, url.Substring(0, url.IndexOf("&sa=")).Replace("/url?q=", ""))
+//    |> Seq.toArray
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
+    for link in links do
+        Console.WriteLine("Caption: {0}\nLink: {1}\n", fst link, snd link)
+    Console.ReadKey() |> ignore
     0 // return an integer exit code
